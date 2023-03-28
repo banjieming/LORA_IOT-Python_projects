3f73d6decdd35aa51160422e0ec8535fb9324df7
Dedizainal <s110003471@g.ksu.edu.tw>
2023-03-28 20:56:36 +0800

    initial commit

---
+++ touch RGB LED on an ESP32 using MicroPython,.py
@@ -0,0 +1,36 @@
+from machine import TouchPad, Pin, PWM
+import time
+
+# Set up the RGB LED pins and touch sensor pins
+red_pin = PWM(Pin(12))
+green_pin = PWM(Pin(13))
+blue_pin = PWM(Pin(14))
+touch_pad = TouchPad(Pin(4))
+
+# Set a threshold value for the touch sensor
+threshold = 200
+
+# Main loop
+while True:
+    # Read the value of the touch sensor
+    touch_value = touch_pad.read()
+
+    # Adjust the RGB LED based on the touch sensor value
+    if touch_value < threshold:
+        # If the touch value is below the threshold, turn the LED red
+        red_pin.duty(512)
+        green_pin.duty(0)
+        blue_pin.duty(0)
+    elif touch_value < (threshold * 2):
+        # If the touch value is between the threshold and twice the threshold, turn the LED green
+        red_pin.duty(0)
+        green_pin.duty(512)
+        blue_pin.duty(0)
+    else:
+        # If the touch value is above twice the threshold, turn the LED blue
+        red_pin.duty(0)
+        green_pin.duty(0)
+        blue_pin.duty(512)
+
+    # Wait for a short period of time before looping again
+    time.sleep(0.1)
